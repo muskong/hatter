eaa77d5f509ed15a963b3bc9eecdb7e722fd031d
Russell <kangyunkui@egeyed.com>
2023-12-07 10:55:26 +0800

    logo

--- routes/api.php
+++ routes/api.php
@@ -18,15 +18,14 @@ use Illuminate\Support\Facades\Route;
 Route::post('/save', ArticleController::class)->name('article.save');
 
 // iA Writer
-Route::get('/ghost/api/v2/admin/site', function(){
+Route::get('/ghost/api/v2/admin/site', function () {
 	return response()->json([
-	    "site"=> [
-			"title"=>"Ghost",
-			"description"=>"The professional publishing platform",
-			"logo"=>"/content/images/2014/09/logo.png",
-			"url"=>"https://hatter.top/api/",
-			"version"=>"3.14"
+		"site" => [
+			"title" => "Hatter",
+			"description" => "The professional publishing platform",
+			"logo" => "https://hatter.top/logo.png",
+			"url" => "https://hatter.top/",
+			"version" => "3.14"
 		]
-
 	]);
 });
