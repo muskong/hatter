f88d34d2578186a15ae656af9759cd1fba326075
Russell <kangyunkui@egeyed.com>
2023-12-07 11:04:46 +0800

    log

---
+++ app/Http/Middleware/LogMiddleware.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class LogMiddleware
+{
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+	 */
+	public function handle(Request $request, Closure $next): Response
+	{
+		logger('请求日志', $request->all());
+
+		return $next($request);
+	}
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ArticleController;
+use App\Http\Middleware\LogMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,18 +15,19 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::middleware([LogMiddleware::class])->group(function () {
+	Route::post('/save', ArticleController::class)->name('article.save');
 
-Route::post('/save', ArticleController::class)->name('article.save');
-
-// iA Writer
-Route::get('/ghost/api/v2/admin/site', function () {
-	return response()->json([
-		"site" => [
-			"title" => "Hatter",
-			"description" => "The professional publishing platform",
-			"logo" => "https://hatter.top/logo.png",
-			"url" => "https://hatter.top/",
-			"version" => "3.14"
-		]
-	]);
+	// iA Writer
+	Route::get('/ghost/api/v2/admin/site', function () {
+		return response()->json([
+			"site" => [
+				"title" => "Hatter.top",
+				"description" => "The professional publishing platform",
+				"logo" => "https://hatter.top/logo.png",
+				"url" => "https://hatter.top/",
+				"version" => "3.14"
+			]
+		]);
+	});
 });
