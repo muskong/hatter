b45283041f8975dca1b3e05432256fe5d716a573
Russell <kangyunkui@egeyed.com>
2023-12-07 10:37:17 +0800

    Update TagController.php

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -10,15 +10,17 @@ class TagController extends Controller
 	/**
 	 * Display a listing of the resource.
 	 */
-	public function __invoke($tag='')
+	public function __invoke($tag = '')
 	{
 		$obj = Tag::with('articles.tags')->firstWhere(['name' => $tag]);
-
+		if (!$obj || !$obj->articles) {
+			return response('error');
+		}
 		$articles = $obj->articles;
 		$article = $articles[0];
 
 		return view('welcome', [
-			'tagName'=>$tag,
+			'tagName' => $tag,
 			'tags' => Tag::tags(),
 			'keywords' => implode(",", array_column($article->tags->toArray(), 'name')),
 			'articles' => $articles,
