c373cee9761fedea058aa6fdd4c1ae13c187d061
Russell <kangyunkui@egeyed.com>
2023-12-07 10:50:56 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,14 @@ Route::post('/save', ArticleController::class)->name('article.save');
 
 // iA Writer
 Route::get('/ghost/api/v2/admin/site', function(){
-	return response('success');
+	return response()->json([
+	    "site"=> [
+			"title"=>"Ghost",
+			"description"=>"The professional publishing platform",
+			"logo"=>"/content/images/2014/09/logo.png",
+			"url"=>"https://hatter.top/api/",
+			"version"=>"3.14"
+		]
+
+	]);
 });
